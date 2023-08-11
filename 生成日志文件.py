91cc0c658296b74fda03114f0e57371dfe40f293
sqcedu <tzxinxiao@163.com>
2023-08-11 13:58:47 +0800

    Create 生成日志文件.py

---
+++ 生成日志文件.py
@@ -0,0 +1,8 @@
+import time, os
+
+new_time = time.strftime("%Y-%m-%d")
+disk_status = os.popen('df -h').readlines()  #readlines
+f = open(new_time+'.log', 'w')
+f.write('%s\n' % disk_status)
+f.flush()
+f.close()
