a51759da740656053f8e7592aeca425cb0767a9c
sqcedu <tzxinxiao@163.com>
2024-05-06 09:50:34 +0800

    Create 排序目录中的文件.py

---
+++ 排序目录中的文件.py
@@ -0,0 +1,12 @@
+# Python脚本,用于根据文件扩展名对目录中的文件进行排序
+import os
+from shutil import move
+ 
+def sort_files(directory_path):
+  for filename in os.listdir(directory_path):
+    if os.path.isfile(os.path.join(directory_path, filename)):
+      file_extension = filename.split('.')[-1]
+      destination_directory = os.path.join(directory_path, file_extension)  
+      if not os.path.exists(destination_directory):
+        os.makedirs(destination_directory)
+      move(os.path.join(directory_path, filename), os.path.join(destination_directory, filename))
